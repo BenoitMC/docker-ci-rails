42083bc579e6dff4e7f95651adbb6dbd238eb24b
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2021-02-19 16:13:09 +0100

    Change docker-build.sh to force rebuild

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,6 @@ set -e
 
 final_tag=$(cat tag.txt)
 tmp_tag="${final_tag}-tmp"
-docker build -t $tmp_tag .
+docker build --pull --no-cache -t $tmp_tag .
 docker tag $tmp_tag $final_tag
 docker rmi $tmp_tag
